10542975fdb4001de668e7bb3e44d1a880386101
kmk216 <kmk216@skbl-422-006.ph.ic.ac.uk>
2019-01-14 16:34:19 +0000

    Introducing cascading delay (2 levels)  before each count

--- Simple1.asm
+++ Simple1.asm
@@ -11,7 +11,7 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	goto	start
 	; ******* My data and where to put it in RAM *
-myTable data	"This is just some data"
+myTable data	"a","b"	;"This is just some data"
 	constant 	myArray=0x400	; Address in RAM for data
 	constant 	counter=0x10	; Address of counter variable
 	; ******* Main programme *********************
@@ -22,20 +22,33 @@ start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM
 	movwf	TBLPTRH		; load high byte to TBLPTRH
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
+	movlw	.2		; 22 bytes to read
 	movwf 	counter		; our counter register
 loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movlw	0xFF
+	movlw	0x03
 	movwf	0x20
+	movwf	0x30
 	call	delay
 	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
 	decfsz	counter		; count down to zero
 	bra	loop		; keep going until finished
 	
-delay	decfsz 0x20
-	bra delay
-	return
 	
-	goto	0
+	goto	0x0	
+
+delay	call delay1
+	movlw	0x03
+	movwf	0x30
+	decfsz 0x20
+	bra delay
+	movlw	0x03
+	movwf	0x20
+	return
+
+delay1	decfsz 0x30
+	bra delay1
+	return
+
+
 
 	end
