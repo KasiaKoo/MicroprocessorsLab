78331dca8fab8f63f088a548ba26f9844d370fd2
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-01-18 12:10:48 +0000

    Update Simple1.asm

--- Simple1.asm
+++ Simple1.asm
@@ -6,16 +6,49 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-start	movlw	0x00
+		
+
+	
+start	movlw	0x00		    ; setting PORTD as only an output
 	movwf	TRISD, ACCESS
 
 	
-loop	movlw	0x55
+	movlw	0x55		    ;choosing the four pins to be high 0,2,4,6
 	movwf	PORTD, ACCESS
+	call write
 	
-	bra	loop
+	
+	
 	
 	goto	0x0
+	
+	
+	
+write		
+	movlw	0x0A
+	
+	clrf	TRISE
+		
+	movwf	LATE, ACCESS
+	
+	movlw	0x51
+	
+	movwf	PORTD, ACCESS
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	movlw	0x55
+	movwf	PORTD, ACCESS
+	
+	setf TRISE
+	
+	return
+	
+	
+
 	
 
 	end
