a4d1507dcd0cdc2f3d2daa61bd7a58ba3f023b6d
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-01-21 08:04:27 +0000

    Update Simple1.asm
    Written up the subroutines for reading and writing to both chips already - we can modify that and comment out the second chip. 
    
    Check with the demonstrator 
    - what is the setf TRISE doing 
    - If we put things on a bus do we have to put it on port as well?

--- Simple1.asm
+++ Simple1.asm
@@ -11,44 +11,91 @@
 	
 start	movlw	0x00		    ; setting PORTD as only an output
 	movwf	TRISD, ACCESS
+	
+	movwf	TRISE, ACCESS		;setting PORTE as only an output
 
 	
 	movlw	0x55		    ;choosing the four pins to be high 0,2,4,6
 	movwf	PORTD, ACCESS
+	
+	movlw 0x0A			;choosing data that we want to put onto memory 1	
+	movwf	0x05			;adress for data in memory 1
+	
+	movlw	0x0B			;choosing data that we want to put on memory 2
+	movwf	0x06			;choosing adress for data for memor 2
+	
 	call write
-	
-	
-	
-	
+	call read
 	goto	0x0
 	
 	
 	
-write		
-	movlw	0x0A
-	
-	clrf	TRISE
+write	
+	;writing to first chip
+	clrf	TRISE			;port E on output
 		
-	movwf	LATE, ACCESS
+	movff	0x05, LATE, ACCESS		;put the data on the bus for mem
 	
-	movlw	0x51
+	;movff	0x05, PORTE			;do we need to set it here after we put it on the bus?
 	
-	movwf	PORTD, ACCESS
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	movlw	0x55
-	movwf	PORTD, ACCESS
+	movlw	0x51			;only cp1 pin off
 	
-	setf TRISE
+	movwf	PORTD, ACCESS		;clock off for 250 ns
+	nop				
+	nop
+	nop
+	nop
+	nop
+	nop
+	movlw	0x55			
+	movwf	PORTD, ACCESS		;clock on
+	
+	;setf TRISE		;so how is the OE off helping to write things?	
+	
+	;writing to second chip
+	
+	clrf	TRISE			;port E on output
+	movff	0x06, LATE, ACCESS		;put the data on the bus for mem
+	;movff	0x06, PORTE			;do we need to set it here after we put it on the bus?
+	movlw	0x15			;only cp1 pin off
+	movwf	PORTD, ACCESS		;clock off for 250 ns
+	nop				
+	nop
+	nop
+	nop
+	nop
+	nop
+	movlw	0x55			
+	movwf	PORTD, ACCESS		;clock on
+	
+	;setf TRISE		;so how is the OE off helping to write things?		
 	
 	return
 	
+read	
+	;reading from first chip
+	movlw 0xFF		    ; Enable Port E input
+	movwf TRISE, ACCESS	
 	
+	movlw  0x54		    ; OE1 Low
+	movwf PORTD
+	
+	movff PORTE,PORTC	    ; read data from port E display on port C
+	
+	movlw  0x55		    ; OE1 High
+	movwf PORTD
 
+	
+	;reading from second chip
+	
+	movlw  0x45		    ; OE2 Low
+	movwf PORTD
+	
+	movff PORTE,PORTJ	    ; Show read data
+	
+	movlw  0x0F		    ; OE2 high
+	movwf PORT
+	movff PORTE,PORTF	    ; read data from port E display on port C
 	
 
 	end
