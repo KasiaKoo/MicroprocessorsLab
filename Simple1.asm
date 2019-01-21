a3ebcfb04607795498cf81d1453d383ed55018b1
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-01-21 17:01:47 +0000

    Update Simple1.asm
    Code for turning 8-bit sequence into serial data. The infinite loop allows us to see the data on external LEDs.

--- Simple1.asm
+++ Simple1.asm
@@ -2,80 +2,43 @@
 	
 	code
 	org 0x0
-	goto	start			;newcode
+	goto	start
 	
 	org 0x100			 ;Main code starts here at address 0x100
 
 		
 
 	
-start	movlw	0x0F			;choosing the four pins to be high 0,2,4,6
+start	movlw	0x5F			;choosing the four pins from previous and RD4 and RD6 on high
 	movwf	PORTD, ACCESS
 	movlw	0x00			
 	movwf	TRISD, ACCESS		;setting PORTD as an output only
-	movwf	TRISE, ACCESS		;setting PORTE as an output only
-	movwf	TRISC, ACCESS		;we set them as output only here cause that is their only function
-	movwf	TRISH, ACCESS
-
-	movlw	0x0B			;choosing data that we want to put into memory 1	
-	movwf	0x05			;choosing address for data in memory 1
-	movlw	0xAA			;choosing data that we want to put into memory 2
-	movwf	0x06			;choosing address for data for memory 2
-	call write
-	call read
-loop	goto	loop
-
 	
-write	;writing to first chip
-	clrf	TRISE			;setting PORTE to the tri state	
-	movff	0x05, LATE		;putting the data on the bus 
-	movlw	0x0D			;only cp1 pin off
-	movwf	PORTD, ACCESS		;clock off for 250 ns
-	nop				
-	nop
-	nop
-	nop
-	nop
-	nop
-	movlw	0x0F			
-	movwf	PORTD, ACCESS		;clock on
-	setf TRISE			
 	
-	;writing to second chip
-	clrf	TRISE			;setting PORTE to the tri state
-	movff	0x06, LATE		;put the data on the bus for mem
-	movlw	0x07			;only cp1 pin off
-	movwf	PORTD, ACCESS		;clock off for 250 ns
-	nop				
-	nop
-	nop
-	nop
-	nop
-	nop
-	movlw	0x0F			
-	movwf	PORTD, ACCESS		;clock on
-	setf TRISE		    ;so how is the OE off helping to write things?		
+	call SPI_MasterInit
 	
+	movlw	0xA0			;8 bit data sequence 
+	call SPI_MasterTransmit
+loop
+	goto	loop
+	
+SPI_MasterInit				; Set clock edge to negative
+	bcf SSP2STAT, CKE
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw (1<<SSPEN)|(1<<CKP)|(0x02)
+	movwf SSP2CON1
+	; SDO2 output; SCK2 output
+	bcf TRISD, SDO2
+	bcf TRISD, SCK2
 	return
 	
-read	;reading from first chip
-	movlw 0xFF		    ;setting PORTE input
-	movwf TRISE, ACCESS	
-	movlw  0x0E		    ;OE1 Low
-	movwf PORTD
-	movff PORTE,PORTC	    ; read data from port E display on port C
-	movlw  0x0F		    ; OE1 High
-	movwf PORTD
-	
-	;reading from second chip
-	movlw  0x0B		    ; OE2 Low
-	movwf PORTD
-	movff PORTE,PORTH	    ; Show read data
-	movlw  0x0F		    ; OE2 high
-	movwf PORTD
-	
+SPI_MasterTransmit			; Start transmission of data (held in W)
+	movwf SSP2BUF
+
+Wait_Transmit				; Wait for transmission to complete
+	btfss PIR2, SSP2IF
+	bra Wait_Transmit
+	bcf PIR2, SSP2IF		; clear interrupt flag
 	return
 	
-finish	nop
 	end
-	
