dcb43ed963568fc448c96681b29046eff45907c8
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-01-21 15:39:52 +0000

    Update Simple1.asm
    Cleaned up the code - commented. 
    Changed the pin values on port d to make it easier for next exercise.

--- Simple1.asm
+++ Simple1.asm
@@ -4,32 +4,32 @@
 	org 0x0
 	goto	start
 	
-	org 0x100		    ; Main code starts here at address 0x100
+	org 0x100			 ;Main code starts here at address 0x100
 
 		
 
 	
-start	movlw	0x00		    ; setting PORTD as only an output
-	movwf	TRISD, ACCESS	
-	movwf	TRISE, ACCESS		;setting PORTE as only an output
-	movlw	0x55		    ;choosing the four pins to be high 0,2,4,6
+start	movlw	0x0F			;choosing the four pins to be high 0,2,4,6
 	movwf	PORTD, ACCESS
-	movlw	0x0B			;choosing data that we want to put onto memory 1	
-	movwf	0x05			;address for data in memory 1
-	movlw	0x0B			;choosing data that we want to put on memory 2
-	movwf	0x06			;choosing address for data for memor 2
+	movlw	0x00			
+	movwf	TRISD, ACCESS		;setting PORTD as an output only
+	movwf	TRISE, ACCESS		;setting PORTE as an output only
+	movwf	TRISC, ACCESS		;we set them as output only here cause that is their only function
+	movwf	TRISH, ACCESS
+
+	movlw	0x0B			;choosing data that we want to put into memory 1	
+	movwf	0x05			;choosing address for data in memory 1
+	movlw	0xAA			;choosing data that we want to put into memory 2
+	movwf	0x06			;choosing address for data for memory 2
 	call write
 	call read
-	;movlw	0x54
-	;movwf	PORTD
-loop	goto loop		
-	goto	0x0
+loop	goto	loop
 
 	
 write	;writing to first chip
-	clrf	TRISE			;port E on output	
-	movff	0x05, LATE		;put the data on the bus for mem
-	movlw	0x51			;only cp1 pin off
+	clrf	TRISE			;setting PORTE to the tri state	
+	movff	0x05, LATE		;putting the data on the bus 
+	movlw	0x0D			;only cp1 pin off
 	movwf	PORTD, ACCESS		;clock off for 250 ns
 	nop				
 	nop
@@ -37,14 +37,14 @@ write	;writing to first chip
 	nop
 	nop
 	nop
-	movlw	0x55			
+	movlw	0x0F			
 	movwf	PORTD, ACCESS		;clock on
 	setf TRISE			
 	
 	;writing to second chip
-	clrf	TRISE			;port E on output
+	clrf	TRISE			;setting PORTE to the tri state
 	movff	0x06, LATE		;put the data on the bus for mem
-	movlw	0x15			;only cp1 pin off
+	movlw	0x07			;only cp1 pin off
 	movwf	PORTD, ACCESS		;clock off for 250 ns
 	nop				
 	nop
@@ -52,25 +52,25 @@ write	;writing to first chip
 	nop
 	nop
 	nop
-	movlw	0x55			
+	movlw	0x0F			
 	movwf	PORTD, ACCESS		;clock on
-	setf TRISE		;so how is the OE off helping to write things?		
+	setf TRISE		    ;so how is the OE off helping to write things?		
 	
 	return
 	
 read	;reading from first chip
-	movlw 0xFF		    ; Enable Port E input
+	movlw 0xFF		    ;setting PORTE input
 	movwf TRISE, ACCESS	
-	movlw  0x54		    ; OE1 Low
+	movlw  0x0E		    ;OE1 Low
 	movwf PORTD
 	movff PORTE,PORTC	    ; read data from port E display on port C
-	movlw  0x55		    ; OE1 High
+	movlw  0x0F		    ; OE1 High
 	movwf PORTD
 	
 	;reading from second chip
-	movlw  0x45		    ; OE2 Low
+	movlw  0x0B		    ; OE2 Low
 	movwf PORTD
-	movff PORTE,PORTF	    ; Show read data
+	movff PORTE,PORTH	    ; Show read data
 	movlw  0x0F		    ; OE2 high
 	movwf PORTD
 	
