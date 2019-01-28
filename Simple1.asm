8463e69dd0eb53f1efca723a4ea74bdca99f373c
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-01-28 17:10:38 +0000

    Update Simple1.asm
    Worked Monday Afternoon - working on dictionary

--- Simple1.asm
+++ Simple1.asm
@@ -1,6 +1,8 @@
 	#include p18f87k22.inc
 
 	extern  Keypad_Setup, Column, Row, Read_Keypad   ;external Keypad subroutines
+	extern  Dic_Setup, Dic_4to2
+	
 
 rst	code	0    ; reset vector
 	goto	setup
@@ -9,14 +11,36 @@ rst	code	0    ; reset vector
 main	code
 	; ******* Programme FLASH read Setup Code ***********************
 setup	call    Keypad_Setup	; setupp Keypad
+	movlw 0x0F
+	movwf 0x021
+	call Column
+
 	goto	start
 	
 	; ******* Main programme ****************************************
 start 	;movlw 0x10
 	;movwf PORTE
 	;call Row
-	call Read_Keypad
 	
+loop	movlw 0x05
+	movwf 0x030
+	movff PORTE, 0x023
+	movf 0x023, W
+	cpfsgt	0x021
+	bra loop
+	call delay
+	movff PORTE, 0x025
+	movf 0x025, W
+	cpfseq	0x023
+	bra loop
+	cpfsgt	0x021
+	bra loop
+	call Read_Keypad
+
 	goto	$		; goto current line in code (stay at this line forever)
+
+delay	decfsz	0x30	; decrement until zero
+	bra delay
+	return
 	
 	end
