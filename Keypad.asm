574dd9978fa1f4f23b470fd944cdb0a8331030b5
kmk216 <kmk216@skbl-422-006.ph.ic.ac.uk>
2019-01-28 12:02:50 +0000

    Creating a Read Keyboard function which reads column and row input and sets it to registry 0x11 - does not decode the number.

--- Keypad.asm
+++ Keypad.asm
@@ -1,30 +1,39 @@
 #include p18f87k22.inc
 
-    global  Keypad_Setup, Con1, Con2, Read_Keypad
+    global  Keypad_Setup, Column, Row, Read_Keypad
 
 Keypad code
     
-Keypad_Setup 
+Keypad_Setup
+	banksel PADCFG1
 	bsf PADCFG1, REPU, BANKED	;set pull ups to on for PORTE
+	movlb 0x00
 	clrf LATE			;writing 0s to the LATE register
 	movlw 0xFF
-	movwf PORTH			;setting PORTH to input
+	movwf TRISH			;setting PORTH to input
 	return
 	
-Con1
+Column
 	movlw 0x0F
 	movwf TRISE, ACCESS			;setting 4-7 as outputs and 0-3 as inputs
 	;may need to add delay here
 	return
 	
-Con2
+Row
 	movlw 0xF0
 	movwf TRISE, ACCESS			;reversing inputs and outputs
 	;may need to add delay here
 	return
 	
 Read_Keypad
-	movff PORTE, PORTH
+	call Column
+	movff PORTE, 0x05
+	call Row
+	movff PORTE, 0x07
+	movf 0x07, W
+	addwf 0x05, W
+	movwf  0x11
+	movwf PORTH, ACCESS
 	return
 
 	end
