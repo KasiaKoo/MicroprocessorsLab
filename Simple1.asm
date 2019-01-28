50f65427b51871daaa424489afd1a4a17cbb8e25
kmk216 <kmk216@skbl-422-006.ph.ic.ac.uk>
2019-01-28 12:03:32 +0000

    Program that puts button pressed to 0x11 register

--- Simple1.asm
+++ Simple1.asm
@@ -1,7 +1,6 @@
 	#include p18f87k22.inc
 
-	extern  Keypad_Setup, Con1, Con2, Read_Keypad   ;external Keypad subroutines
-	
+	extern  Keypad_Setup, Column, Row, Read_Keypad   ;external Keypad subroutines
 
 rst	code	0    ; reset vector
 	goto	setup
@@ -9,16 +8,14 @@ rst	code	0    ; reset vector
 
 main	code
 	; ******* Programme FLASH read Setup Code ***********************
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	call    Keypad_Setup	; setupp Keypad
+setup	call    Keypad_Setup	; setupp Keypad
 	goto	start
 	
 	; ******* Main programme ****************************************
 start 	;movlw 0x10
 	;movwf PORTE
-	call Con1
-	;call Read_Keypad
+	;call Row
+	call Read_Keypad
 	
 	goto	$		; goto current line in code (stay at this line forever)
 	
