7f28c0dfc7fb389af4d0108475a80f572eddb4ea
kmk216 <kmk216@skbl-422-006.ph.ic.ac.uk>
2019-02-18 16:06:00 +0000

    Changed logic of PWM period (dc constant, period constant and remainder of time). Counter changes speed of rotation. Now motor is on continuous rotation between 0 and 180 degrees.

--- PWM_long.asm
+++ PWM_long.asm
@@ -1,6 +1,6 @@
     #include p18f87k22.inc
     
-    global  PWM_Setup, PWM_dc, PWM_test_routine
+    global  PWM_Setup, PWM_dc, PWM_test_routine, PWM_cycle_big2
     
 acs0    udata_acs   ; named variables in access ram
 PWM_cnt_l   res 1   ; reserve 1 byte for variable PWM_cnt_l
@@ -9,9 +9,14 @@ PWM_cnt_h   res 1   ; reserve 1 byte for variable PWM_cnt_h
 PWM_cnt_b   res	4   ; reserve 4 bytes for big PWM cycle
 PWM_cnt_m   res	4   ; reserve 4 bytes for mid PWM cycle
 PWM_cnt_s   res	4   ; reserve 4 bytes for small PWM cycle
-PWM_cnt_ms  res 1   ; reserve 1 byte for ms counter
+PWM_cnt_b2   res	4   ; reserve 4 bytes for big PWM cycle
+PWM_cnt_m2   res	4   ; reserve 4 bytes for mid PWM cycle
+PWM_cnt_s2   res	4   ; reserve 4 bytes for small PWM cycle
+
 PWM_pr	    res 4   ; reserve 4 bytes for PWM period
+PWM_pr_con  res 4
 PWM_dc	    res 4   ; reserve 4 bytes for PWM duty cycle
+PWM_dc_con  res 4
 counter	    res	4   ; reserve 4 bytes for test routine
 
 PWM_counter res 2   ; reserve 2 bytes
@@ -23,15 +28,19 @@ PWM_Setup
 	clrf TRISE	    
 	clrf PORTE
 	clrf LATE	    ; PORTE as PWM motor cycle
-	movlw	0xC5
-	movwf	PWM_pr	    ; setting PWM period length to 20 ms
+	movlw	0xAA
+	movwf	PWM_pr_con	    ; setting PWM period length to 20 ms
+	movlw	0x58
+	movwf	PWM_pr
 	movlw   0x05
 	movwf   PWM_counter
-	movlw	0x06
+	movlw   0x05
+	movwf   PWM_dc_con
+	movlw	0x04
 	movwf	PWM_dc      ; setting minimum duty cycle length to 1 ms
-	movlw	0x02
+	movlw	0x20
 	movwf	counter    
-	return
+	return                    
 	
 ;PWM_long
 	;movlw	0x01
@@ -58,55 +67,72 @@ PWM_Setup
 	;goto loop2
 	
 PWM_test_routine
+	movlw	0x00
+	cpfsgt	counter
+	call	counter_reset
 	decf	counter
 	movlw	0xFF
 	movwf	PORTE
+	movf    PWM_dc_con, W
+	call    PWM_cycle_big
 	movf	PWM_dc, W
-	call	PWM_cycle_big
+	call	PWM_cycle_big2
 	movlw	0x00
 	movwf	PORTE
 	movf	PWM_dc, W
 	subwf	PWM_pr, W
+	call	PWM_cycle_big2
+	movf	PWM_pr_con, W
 	call	PWM_cycle_big
-	movlw	0x00
+	movlw	0x01
+	movff	counter, PORTH
 	cpfsgt	counter
 	incf	PWM_dc
-	movlw	0x11
+	movlw	0x57
 	cpfseq	PWM_dc
 	bra	PWM_test_routine
+
 reset_dc    
-	movlw	0x0A
+	movlw	0x04
 	movwf	PWM_dc
 	bra	PWM_test_routine
 	
+counter_reset
+	movlw	0x20
+	movwf	counter
+	return
 	
-;PWM_delay_ms		    ; delay given in ms in W
-	;movwf	PWM_cnt_ms
-;pwmlp2	;movlw	.125	    ; 1 ms delay
-	;call	PWM_delay_x4us	
-	;decfsz	PWM_cnt_ms
-	;bra	pwmlp2
-	;return
-    
-;PWM_delay_x4us		    ; delay given in chunks of 4 microsecond in W
-	;movwf	PWM_cnt_l   ; now need to multiply by 16
-	;swapf   PWM_cnt_l,F ; swap nibbles
-	;movlw	0x0f	    
-	;andwf	PWM_cnt_l,W ; move low nibble to W
-	;movwf	PWM_cnt_h   ; then to PWM_cnt_h
-	;movlw	0xf0	    
-	;andwf	PWM_cnt_l,F ; keep high nibble in PWM_cnt_l
-	;call	PWM_delay
-	;return
-
-;PWM_delay			; delay routine	4 instruction loop == 250ns	    
-	;movlw 	0x00		; W=0
-;pwmlp1	decf 	PWM_cnt_l,F	; no carry when 0x00 -> 0xff
-	;subwfb 	PWM_cnt_h,F	; no carry when 0x00 -> 0xff
-	;bc 	pwmlp1		; carry, then loop again
-	;return			; carry reset so return
+	
+;~~~~~~~~The delay used for the duty cycle of the motor~~~~~~~~~~~~~~~~~~~
+PWM_cycle_big2
+	movwf	PWM_cnt_b2   ; counting down from number specified in test routine depending on duty cycle length
+big2	movlw	0x00
+	cpfseq	PWM_cnt_b2   ; skip next instruction and return if counter has reached 0
+	goto	PWM_mid2   ; delay of 0.1 ms 
+	return
+	
+	
+PWM_mid2
+	movlw	0x05
+	movwf	PWM_cnt_m2
+mid2	decf	PWM_cnt_m2   ; counting down from 9
+	movlw	0x00
+	cpfseq	PWM_cnt_m2   ; skip next instruction and branch to big cycle if counter has reached 0
+	goto	PWM_small2
+	decf	PWM_cnt_b2
+	bra	big2
+	
+PWM_small2	
+	movlw	0x10
+	movwf	PWM_cnt_s2
+small2	decf	PWM_cnt_s2   ; counting down from 39
+	movlw	0x00
+	cpfseq	PWM_cnt_s2   ; skip next instruction and branch to mid cycle if counter has reached 0
+	bra	small2
+	bra	mid2
 
 	
+;~~~~~~~~The delay used for 17 ms of the period for the motor~~~~~~~~~~~~~~~~~~~	
 PWM_cycle_big
 	movwf	PWM_cnt_b   ; counting down from number specified in test routine depending on duty cycle length
 big	movlw	0x00
--- Simple1.asm
+++ Simple1.asm
@@ -1,6 +1,6 @@
 	#include p18f87k22.inc
 	
-	extern PWM_Setup, PWM_dc, PWM_test_routine
+	extern PWM_Setup, PWM_dc, PWM_test_routine, PWM_cycle_big2
 	extern  ADC_Setup, ADC_Read
 	extern LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_clear, LCD_delay_ms
 	
@@ -47,6 +47,12 @@ setup
 	clrf TRISH
 	clrf PORTH  ;setting port G as output for int_cnt testing
 	
+	;movlw 0x01
+	;movwf PORTH
+	;movlw 0x01
+	;call PWM_cycle_big2
+	;movlw 0xFF
+	;movwf PORTH
 	call PWM_test_routine
 	
 	;movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
