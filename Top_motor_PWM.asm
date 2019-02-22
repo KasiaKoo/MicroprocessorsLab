94f2107c65668ab96baf3325179337deb259793d
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-02-22 08:07:55 +0000

    Update Top_motor_PWM.asm
    Made the change in the end of the rotate routine - does not change dc but goes through the counter and only goes back when counter is 0; cleaned the code

--- Top_motor_PWM.asm
+++ Top_motor_PWM.asm
@@ -2,73 +2,82 @@
     
     global PWM_Setup_T, PWM_dc_T, PWM_rotate_T, PWM_delay_T
     
-bnk0    udata	0x100   ; named variables in access ram
-;~~~~~~~ Counters used in the delay
-PWM_cnt_b   res	4   ; reserve 4 bytes for big PWM cycle
-PWM_cnt_m   res	4   ; reserve 4 bytes for mid PWM cycle
-PWM_cnt_s   res	4   ; reserve 4 bytes for small PWM cycle
-PWM_cnt_b2   res	4   ; reserve 4 bytes for big PWM cycle
-PWM_cnt_m2   res	4   ; reserve 4 bytes for mid PWM cycle
-PWM_cnt_s2   res	4   ; reserve 4 bytes for small PWM cycle
+bnk0    udata	0x100   ; named variables in Bank 1 
+;~~~~~~~ Counters used in delays
+cnt_con	    res	4   	; reserve 4 bytes for big PWM cycle
+cnt_100um   res	4   	; reserve 4 bytes for mid PWM cycle
+cnt_10um    res	4   	; reserve 4 bytes for small PWM cycle
+cnt_var     res 4   	; reserve 4 bytes for big PWM cycle
+cnt_20um    res	4   	; reserve 4 bytes for mid PWM cycle
+cnt_4um     res	4   	; reserve 4 bytes for small PWM cycle
 
 ;~~~~~~~ PWM period and duty cycle values
-PWM_pr	    res 4   ; reserve 4 bytes for PWM period
-PWM_dc_T    res 4   ; reserve 4 bytes for PWM duty cycle
+PWM_pr	    res 4   ; reserve 4 bytes for PWM period remainder
+PWM_dc_T    res 4   ; reserve 4 bytes for PWM duty cycle remainder
 counter	    res	4   ; reserve 4 bytes for test routine
-PWM_counter res 2   ; reserve 2 bytes
-PWM_dc_con  res 4
-PWM_pr_con  res 4
+PWM_counter res 2   ; reserve 2 bytes !!!!! Check if we use that
+PWM_dc_con  res 4   ; reserve 4 bytes for PWM period
+PWM_pr_con  res 4   ; reserve 4 bytes for PWM duty cycle
   
+;................THE PWM FOR SG90 MOTOR..................................................
+;Motor's datasheet requires 20 ms period which is not compatible with built in PWM module
+;Therefore, we created our own module which takes care of this PWM through the use of delays
+;The 20 ms period is split into 0.5 ms constant duty cycle, remainder and 17 ms constant period
+;The remainder allows us to control the length of the of the duty cycle by dividing it 
+;into period and the duty cycle in the rotation routine
+;The remainder is not set exactly for 2.5 ms but with length calculated with oscillopscope
+;that takes into account the extra clock cycle used during rotation commands
+;the counter controls the number of complete pulses which are sent to the motor before 
+;any changes to dc are made
+;........................................................................................
 
 PWM	code
- 
+
+;~~~~~~ Top Moto PWM setup ~~~~~~~~~~~~~~~~~~~~~~~
+
 PWM_Setup_T
 	movlw	0xAA
-	movwf	PWM_pr_con	    ; setting PWM period length to 20 ms
+	movwf	PWM_pr_con, BANKED		; setting PWM constant period length to 17 ms
 	movlw   0x05
-	movwf   PWM_dc_con
+	movwf   PWM_dc_con, BANKED		; setting the minimum duty cycle to 0.5 ms
 	movlw	0x58
-	movwf	PWM_pr, BANKED
+	movwf	PWM_pr, BANKED			; setting the Reminder to ~2 ms 
 	movlw   0x05
-	movwf   PWM_counter, BANKED
+	movwf   PWM_counter, BANKED		; !!!!! I Do not think we use it
 	movlw	0x04
-	movwf	PWM_dc_T, BANKED      ; setting minimum duty cycle length to 1 ms
+	movwf	PWM_dc_T, BANKED		; setting variable dc so that the motor is comfortable
 	movff	PWM_dc_T, PORTH
 	movlw	0x20
-	movwf	counter, BANKED    
+	movwf	counter, BANKED    		;controls the speed of the rotation
 	return                    
 
-	
-PWM_rotate_T
-	movlw	0x00
-	cpfsgt	counter, BANKED
-	call	counter_reset
-	decf	counter, BANKED
-	bsf	PORTE, 4
-	movf    PWM_dc_con, W, BANKED
-	call    PWM_cycle_big
-	movf	PWM_dc_T, W, BANKED
-	call	PWM_delay_T, BANKED
-	bcf	PORTE, 4
-	movf	PWM_dc_T, W, BANKED
-	subwf	PWM_pr, W, BANKED
-	call	PWM_delay_T
-	movf	PWM_pr_con, W, BANKED
-	call	PWM_cycle_big
-	movlw	0x00
-	cpfsgt	counter, BANKED
-	return
-	incf	PWM_dc_T
-	movlw	0x57
-	cpfseq	PWM_dc_T, BANKED
-	return
-	return
-	;bra	PWM_rotate_T
+;~~~~~~ Top Moto PWM setup ~~~~~~~~~~~~~~~~~~~~~~~
 
-reset_dc    
-	movlw	0x04
-	movwf	PWM_dc_T, BANKED
-	bra	PWM_rotate_T
+PWM_rotate_T
+						 
+	movlw	0x00
+	cpfsgt	counter, BANKED			;checking if counter reached 0
+	call	counter_reset			;reset counter if 0
+	decf	counter, BANKED			;decrement counter if not 0
+	
+	bsf	PORTE, 4			;setting the pulse to high on RE4
+	movf    PWM_dc_con, W, BANKED		
+	call    PWM_100um			;implementing constant part of the duty cycle
+	movf	PWM_dc_T, W, BANKED
+	call	PWM_delay_T, BANKED		;implementing the variable part of the duty cycle
+	
+	bcf	PORTE, 4			;setting the pulse to low on RE4
+	movf	PWM_dc_T, W, BANKED
+	subwf	PWM_pr, W, BANKED		;finding how much remainder has to be implemented after this duty cycle
+	call	PWM_delay_T			;implementing the remainder
+	movf	PWM_pr_con, W, BANKED
+	call	PWM_100um			;implementing the constant part of the period
+	
+	movlw	0x00				
+	cpfsgt	counter, BANKED			;checking if the counter is zero
+	return					;if yes return from the routine
+	bra	PWM_rotate_T			;i no repeat the routine
+
 	
 counter_reset
 	movlw	0x20
@@ -78,60 +87,60 @@ counter_reset
 	
 ;~~~~~~~~The delay used for the duty cycle of the motor~~~~~~~~~~~~~~~~~~~
 PWM_delay_T
-	movwf	PWM_cnt_b2, BANKED   ; counting down from number specified in test routine depending on duty cycle length
-big2	movlw	0x00
-	cpfseq	PWM_cnt_b2, BANKED   ; skip next instruction and return if counter has reached 0
-	goto	PWM_mid2   ; delay of 0.1 ms 
+	movwf	cnt_var, BANKED   ; counting down from number specified in rotate depending on duty cycle length
+Var_d	movlw	0x00
+	cpfseq	cnt_var, BANKED   ; skip next instruction and return if counter has reached 0
+	goto	10um_delay   ; delay of 0.1 ms 
 	return
 	
 	
-PWM_mid2
+20um_delay
 	movlw	0x05
-	movwf	PWM_cnt_m2, BANKED
-mid2	decf	PWM_cnt_m2, BANKED   ; counting down from 9
+	movwf	cnt_20um, BANKED
+20_d	decf	cnt_20um, BANKED   ; counting down from 9
 	movlw	0x00
-	cpfseq	PWM_cnt_m2, BANKED   ; skip next instruction and branch to big cycle if counter has reached 0
-	goto	PWM_small2
-	decf	PWM_cnt_b2, BANKED
-	bra	big2
+	cpfseq	cnt_20um, BANKED   ; skip next instruction and branch to big cycle if counter has reached 0
+	goto	4um_delay
+	decf	cnt_var, BANKED
+	bra	Var_d
 	
-PWM_small2	
+4um_delay	
 	movlw	0x10
-	movwf	PWM_cnt_s2, BANKED
-small2	decf	PWM_cnt_s2, BANKED   ; counting down from 39
+	movwf	cnt_4um, BANKED
+4_d	decf	cnt_4um, BANKED   ; counting down from 39
 	movlw	0x00
-	cpfseq	PWM_cnt_s2, BANKED   ; skip next instruction and branch to mid cycle if counter has reached 0
-	bra	small2
-	bra	mid2
+	cpfseq	cnt_4um, BANKED   ; skip next instruction and branch to mid cycle if counter has reached 0
+	bra	4_d
+	bra	20_d
 
 	
 ;~~~~~~~~The delay used for 17 ms of the period for the motor~~~~~~~~~~~~~~~~~~~	
-PWM_cycle_big
-	movwf	PWM_cnt_b , BANKED  ; counting down from number specified in test routine depending on duty cycle length
-big	movlw	0x00
-	cpfseq	PWM_cnt_b, BANKED   ; skip next instruction and return if counter has reached 0
-	goto	PWM_mid  ; delay of 0.1 ms 
+PWM_100um
+	movwf	cnt_con , BANKED  ; counting down from number specified in rotate
+Con_d	movlw	0x00
+	cpfseq	cnt_con, BANKED   ; skip next instruction and return if counter has reached 0
+	goto	10um_delay  ; delay of 0.1 ms 
 	return
 	
 	
-PWM_mid
+100um_delay
 	movlw	0x09
-	movwf	PWM_cnt_m, BANKED
-mid	decf	PWM_cnt_m, BANKED   ; counting down from 9
+	movwf	cnt_100um, BANKED
+100_d	decf	cnt_100um, BANKED   ; counting down from 9
 	movlw	0x00
-	cpfseq	PWM_cnt_m, BANKED   ; skip next instruction and branch to big cycle if counter has reached 0
-	goto	PWM_small
-	decf	PWM_cnt_b, BANKED
-	bra	big
+	cpfseq	cnt_100um, BANKED   ; skip next instruction and branch to big cycle if counter has reached 0
+	goto	10um_delay
+	decf	cnt_con, BANKED
+	bra	Con_d
 	
-PWM_small	
+10um_delay	
 	movlw	0x27
-	movwf	PWM_cnt_s, BANKED
-small	decf	PWM_cnt_s, BANKED   ; counting down from 39
+	movwf	cnt_10um, BANKED
+10_d	decf	cnt_10um, BANKED   ; counting down from 39
 	movlw	0x00
-	cpfseq	PWM_cnt_s, BANKED   ; skip next instruction and branch to mid cycle if counter has reached 0
-	bra	small
-	bra	mid
+	cpfseq	cnt_10um, BANKED   ; skip next instruction and branch to mid cycle if counter has reached 0
+	bra	10_d
+	bra	100_d
 	
 	end
 
