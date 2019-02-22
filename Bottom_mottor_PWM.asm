217d7d94e518f60a74d65ea927bb84951e2dbe4b
KasiaKoo <46668786+KasiaKoo@users.noreply.github.com>
2019-02-22 08:25:39 +0000

    Update Bottom_mottor_PWM.asm
    Cleaning the code

--- Bottom_mottor_PWM.asm
+++ Bottom_mottor_PWM.asm
@@ -4,66 +4,79 @@
     
 acs0    udata_acs   ; named variables in access ram
 ;~~~~~~~ Counters used in the delay    
-PWM_cnt_b   res	4   ; reserve 4 bytes for big PWM cycle
-PWM_cnt_m   res	4   ; reserve 4 bytes for mid PWM cycle
-PWM_cnt_s   res	4   ; reserve 4 bytes for small PWM cycle
-PWM_cnt_b2   res	4   ; reserve 4 bytes for big PWM cycle
-PWM_cnt_m2   res	4   ; reserve 4 bytes for mid PWM cycle
-PWM_cnt_s2   res	4   ; reserve 4 bytes for small PWM cycle
+cnt_con		res 4   ; reserve 4 bytes for big PWM cycle
+cnt_100um	res 4   ; reserve 4 bytes for mid PWM cycle
+cnt_10um	res 4   ; reserve 4 bytes for small PWM cycle
+cnt_var		res 4   ; reserve 4 bytes for big PWM cycle
+cnt_20um	res 4   ; reserve 4 bytes for mid PWM cycle
+cnt_4um   	res 4   ; reserve 4 bytes for small PWM cycle
 
 ;~~~~~~~ PWM period and duty cycle values
-PWM_pr	    res 4   ; reserve 4 bytes for PWM period
-PWM_pr_con  res 4
-PWM_dc_B    res 4   ; reserve 4 bytes for PWM duty cycle
-PWM_dc_con  res 4
-counter	    res	4   ; reserve 4 bytes for test routine
-PWM_counter res 2   ; reserve 2 bytes
+PWM_pr	    res 4	; reserve 4 bytes for PWM period remainder
+PWM_pr_con  res 4	; reserve 4 bytes for PWM period
+PWM_dc_B    res 4	; reserve 4 bytes for PWM duty cycle remainder
+PWM_dc_con  res 4	; reserve 4 bytes for PWM duty cycl
+counter	    res	4	; reserve 4 bytes for test routine
+PWM_counter res 2   	; reserve 2 bytes !!!!! I do not think we need it
   
+;................THE PWM FOR SG90 MOTOR..................................................
+;Motor's datasheet requires 20 ms period which is not compatible with built in PWM module
+;Therefore, we created our own module which takes care of this PWM through the use of delays
+;The 20 ms period is split into 0.5 ms constant duty cycle, remainder and 17 ms constant period
+;The remainder allows us to control the length of the of the duty cycle by dividing it 
+;into period and the duty cycle in the rotation routine
+;The remainder is not set exactly for 2.5 ms but with length calculated with oscillopscope
+;that takes into account the extra clock cycle used during rotation commands
+;the counter controls the number of complete pulses which are sent to the motor before 
+;any changes to dc are made
+;........................................................................................
 
 PWM	code
  
 PWM_Setup_B
 	clrf TRISE	    
 	clrf PORTE
-	clrf LATE	    ; PORTE as PWM motor cycle
+	clrf LATE	    		; PORTE as PWM motor cycle (as output)
 	movlw	0xAA
-	movwf	PWM_pr_con	    ; setting PWM period length to 20 ms
+	movwf	PWM_pr_con		; setting PWM period length to 17 ms
 	movlw	0x58
-	movwf	PWM_pr
+	movwf	PWM_pr			; setting the Reminder to ~2 ms 
 	movlw   0x05
-	movwf   PWM_counter
+	movwf   PWM_counter		;!!!!! I do not think we use this
 	movlw   0x05
-	movwf   PWM_dc_con
+	movwf   PWM_dc_con		; setting the minimum duty cycle to 0.5 ms
 	movlw	0x04
-	movwf	PWM_dc_B      ; setting minimum duty cycle length to 1 ms
+	movwf	PWM_dc_B      		; setting variable dc so that the motor is comfortable
 	movlw	0x20
-	movwf	counter    
+	movwf	counter    		;setting the number of pulses send to the motor with one dc
 	return                    
 
 	
 PWM_rotate_B
 	movlw	0x00
-	cpfsgt	counter
-	call	counter_reset
-	decf	counter
-	bsf	PORTE, 0
+	cpfsgt	counter, BANKED			;checking if counter reached 0
+	call	counter_reset			;reset counter if 0
+	decf	counter, BANKED			;decrement counter if not 0
+	
+	bsf	PORTE, 0			;setting the pulse to high on RE4
 	movf    PWM_dc_con, W
-	call    PWM_cycle_big
+	call    PWM_100um			;implementing constant part of the duty cycle
 	movf	PWM_dc_B, W
-	call	PWM_delay_B
-	bcf	PORTE, 0
+	call	PWM_delay_B			;implementing the variable part of the duty cycle
+	
+	bcf	PORTE, 0			;setting the pulse to low on RE4
 	movf	PWM_dc_B, W
-	subwf	PWM_pr, W
-	call	PWM_delay_B
-	movf	PWM_pr_con, W
-	call	PWM_cycle_big
+	subwf	PWM_pr, W			;finding how much remainder has to be implemented after this duty cycle
+	call	PWM_delay_B			;implementing the remainder
+	movf	PWM_pr_con, W			
+	call	PWM_100um			;implementing the constant part of the period
+	
 	movlw	0x00
-	cpfsgt	counter
-	return
-	;incf	PWM_dc
-	;movlw	0x57
-	cpfseq	PWM_dc_B
-	bra	PWM_rotate_B
+	cpfsgt	counter				;checks if counter reached 0
+	return					;if yes return to the interupt
+	cpfseq	PWM_dc_B			;if no compare dc_to zero
+	bra	PWM_rotate_B			;return to the loop
+
 
 reset_dc    
 	movlw	0x04
@@ -78,60 +91,60 @@ counter_reset
 	
 ;~~~~~~~~The delay used for the duty cycle of the motor~~~~~~~~~~~~~~~~~~~
 PWM_delay_B
-	movwf	PWM_cnt_b2   ; counting down from number specified in test routine depending on duty cycle length
-big2	movlw	0x00
-	cpfseq	PWM_cnt_b2   ; skip next instruction and return if counter has reached 0
-	goto	PWM_mid2   ; delay of 0.1 ms 
+	movwf	cnt_var   ; counting down from number specified in test routine depending on duty cycle length
+Var_d	movlw	0x00
+	cpfseq	cnt_var   ; skip next instruction and return if counter has reached 0
+	goto	20um_delay  ; delay of 0.1 ms 
 	return
 	
 	
-PWM_mid2
+20um_delay
 	movlw	0x05
-	movwf	PWM_cnt_m2
-mid2	decf	PWM_cnt_m2   ; counting down from 9
+	movwf	cnt_20um
+20_d	decf	cnt_20um   ; counting down from 9
 	movlw	0x00
-	cpfseq	PWM_cnt_m2   ; skip next instruction and branch to big cycle if counter has reached 0
-	goto	PWM_small2
-	decf	PWM_cnt_b2
-	bra	big2
+	cpfseq	cnt_20um   ; skip next instruction and branch to big cycle if counter has reached 0
+	goto	4um_delay
+	decf	cnt_var
+	bra	Var_d
 	
-PWM_small2	
+4um_delay	
 	movlw	0x10
-	movwf	PWM_cnt_s2
-small2	decf	PWM_cnt_s2   ; counting down from 39
+	movwf	cnt_4um
+4_d	decf	cnt_4um   ; counting down from 39
 	movlw	0x00
-	cpfseq	PWM_cnt_s2   ; skip next instruction and branch to mid cycle if counter has reached 0
-	bra	small2
-	bra	mid2
+	cpfseq	cnt_4um   ; skip next instruction and branch to mid cycle if counter has reached 0
+	bra	4_d
+	bra	20_d
 
 	
 ;~~~~~~~~The delay used for 17 ms of the period for the motor~~~~~~~~~~~~~~~~~~~	
-PWM_cycle_big
-	movwf	PWM_cnt_b   ; counting down from number specified in test routine depending on duty cycle length
-big	movlw	0x00
-	cpfseq	PWM_cnt_b   ; skip next instruction and return if counter has reached 0
-	goto	PWM_mid   ; delay of 0.1 ms 
+PWM_100um
+	movwf	cnt_con   ; counting down from number specified in test routine depending on duty cycle length
+Con_d	movlw	0x00
+	cpfseq	cnt_con   ; skip next instruction and return if counter has reached 0
+	goto	100m_delay   ; delay of 0.1 ms 
 	return
 	
 	
-PWM_mid
+100m_delay
 	movlw	0x09
-	movwf	PWM_cnt_m
-mid	decf	PWM_cnt_m   ; counting down from 9
+	movwf	cnt_100um
+100_d	decf	cnt_100um   ; counting down from 9
 	movlw	0x00
-	cpfseq	PWM_cnt_m   ; skip next instruction and branch to big cycle if counter has reached 0
-	goto	PWM_small
-	decf	PWM_cnt_b
-	bra	big
+	cpfseq	cnt_100um   ; skip next instruction and branch to big cycle if counter has reached 0
+	goto	10um_delay
+	decf	cnt_con
+	bra	Con_d
 	
-PWM_small	
+10um_delay	
 	movlw	0x27
-	movwf	PWM_cnt_s
-small	decf	PWM_cnt_s   ; counting down from 39
+	movwf	cnt_10um
+10_d	decf	cnt_10um   ; counting down from 39
 	movlw	0x00
-	cpfseq	PWM_cnt_s   ; skip next instruction and branch to mid cycle if counter has reached 0
-	bra	small
-	bra	mid
+	cpfseq	cnt_10um   ; skip next instruction and branch to mid cycle if counter has reached 0
+	bra	10_d
+	bra	100_d
 	
 	end
 
